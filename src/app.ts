4a0739692e66f7585975794848663568fdab1db1
newbee1939 <hideto3970@gmail.com>
2022-12-12 08:37:33 +0900

    仮実装

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { App } from "@slack/bolt";
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+// Listens to incoming messages that contain "hello"
+app.message("hello", async ({say}) => {
+  // say() sends a message to the channel where the event was triggered
+  await say("Hello!");
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log("⚡️ Bolt app is running!");
+})();
