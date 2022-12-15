ec261f25086a6b55883679e2db404732881196a6
newbee1939 <hideto3970@gmail.com>
2022-12-15 21:23:53 +0900

    不要な実装を削除

--- src/app.ts
+++ src/app.ts
@@ -6,31 +6,18 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-// これで良さそう
 app.event("app_mention", async({say}) => {
-  // OpenAIの処理
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
   const response = await openai.listEngines();
+
+  // Requestを作る
+  
   console.log(response);
 
-  await say("メンションされたので返信!");
-});
-
-// Listens to incoming messages that contain "hello"
-// @slac-testへのメンションがあったときだけ動くようにする
-app.message("hello", async ({say}) => {
-  console.log("helloに来たよ！！");
-  // say() sends a message to the channel where the event was triggered
-
-  await say("Hello!");
-});
-
-app.command(`/echo`, async ({ command, ack, say }) => {
-  ack()
-  say(`発言: ${command.text}`)
+  await say("メンションされたので返信!!");
 });
 
 (async () => {
