ae4aaeb52984f3a9cd6d0e12ffe40ecfeae1f067
newbee1939 <hideto3970@gmail.com>
2023-01-06 12:43:59 +0900

    format

--- src/app.ts
+++ src/app.ts
@@ -3,27 +3,27 @@ import { Configuration, OpenAIApi } from "openai";
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
 // TODO: messageを受け取るようにする
 // TODO: 文字数はどうする？制限無くした方が良さそう
-app.event("app_mention", async({say}) => {
+app.event("app_mention", async ({ say }) => {
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
-  
+
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: "プログラミングの上達方法を教えてください",
     temperature: 0,
     max_tokens: 500,
   });
-  
+
   const responseText = response.data.choices[0].text;
   if (responseText === undefined) {
-    await say("ごめんなさい。。わからにゃい。。。")
+    await say("ごめんなさい。。わからにゃい。。。");
     return;
   }
   // TODO:なぜか二回動いてしまうので解消する！（これ：https://dev.classmethod.jp/articles/slack-resend-matome/）
